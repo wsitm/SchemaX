32c8f411f23129c82333f9e4af55ecd56ceda440
wsitm <392079454@qq.com>
2025-04-04 10:17:35 +0800

    1、优化代码

--- wsitm-ui/src/plugins/index.js
+++ wsitm-ui/src/plugins/index.js
@@ -2,6 +2,11 @@ import cache from './cache'
 import modal from './modal'
 import download from './download'
 
+// 禁用react devtools，因为 univerjs 间接引用了 react-devtools-core，导致开发时阻塞卡顿
+if (process.env.NODE_ENV === 'development') {
+  window.__REACT_DEVTOOLS_GLOBAL_HOOK__ = { isDisabled: true };
+}
+
 export default {
   install(Vue) {
     // 缓存对象
