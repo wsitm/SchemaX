e1e7eb4ef35c4421b2329365c6d8f10ad60eaab2
wsitm <392079454@qq.com>
2026-02-26 11:47:14 +0800

    feat(json): 扩展JSONArray和JSONObject的数据类型转换功能

--- schemax-code/src/main/java/org/wsitm/schemax/utils/json/JSONArray.java
+++ schemax-code/src/main/java/org/wsitm/schemax/utils/json/JSONArray.java
@@ -2,8 +2,14 @@ package org.wsitm.schemax.utils.json;
 
 import org.wsitm.schemax.utils.JsonUtil;
 
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.temporal.TemporalAccessor;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.UUID;
 
 public class JSONArray extends ArrayList<Object> {
     public JSONArray() {
@@ -37,23 +43,190 @@ public class JSONArray extends ArrayList<Object> {
     }
 
     public String getString(int index) {
-        Object value = getOrNull(index);
-        return value == null ? null : String.valueOf(value);
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof String) {
+            return (String) value;
+        } else if (value instanceof Date) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            return sdf.format((Date) value);
+        } else {
+            return !(value instanceof Boolean)
+                    && !(value instanceof Character)
+                    && !(value instanceof Number)
+                    && !(value instanceof UUID)
+                    && !(value instanceof Enum)
+                    && !(value instanceof TemporalAccessor) ? JsonUtil.toJSONString(value) : value.toString();
+        }
     }
 
-    public Integer getInteger(int index) {
-        Object value = getOrNull(index);
-        return value == null ? null : JsonUtil.toJavaObject(value, Integer.class);
+    public Double getDouble(int index) {
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Double) {
+            return (Double) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).doubleValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? Double.parseDouble(str) : null;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to double");
+        }
     }
+
+    public double getDoubleValue(int index) {
+        Double value = this.getDouble(index);
+        return value == null ? (double) 0.0F : value;
+    }
+
+    public Float getFloat(int index) {
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Float) {
+            return (Float) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).floatValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? Float.parseFloat(str) : null;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to float");
+        }
+    }
+
+    public float getFloatValue(int index) {
+        Float value = this.getFloat(index);
+        return value == null ? 0.0F : value;
+    }
+
 
     public Long getLong(int index) {
-        Object value = getOrNull(index);
-        return value == null ? null : JsonUtil.toJavaObject(value, Long.class);
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Long) {
+            return (Long) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).longValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            if (!str.isEmpty() && !"null".equalsIgnoreCase(str)) {
+                return str.indexOf(46) != -1 ? (long) Double.parseDouble(str) : Long.parseLong(str);
+            } else {
+                return null;
+            }
+        } else if (value instanceof Boolean) {
+            return (Boolean) value ? 1L : 0L;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to Long");
+        }
+    }
+
+    public long getLongValue(int index) {
+        Long value = this.getLong(index);
+        return value == null ? 0L : value;
+    }
+
+
+    public Integer getInteger(int index) {
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Integer) {
+            return (Integer) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).intValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            if (!str.isEmpty() && !"null".equalsIgnoreCase(str)) {
+                return str.indexOf(46) != -1 ? (int) Double.parseDouble(str) : Integer.parseInt(str);
+            } else {
+                return null;
+            }
+        } else if (value instanceof Boolean) {
+            return (Boolean) value ? 1 : 0;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to Integer");
+        }
+    }
+
+    public int getIntValue(int index) {
+        Integer value = this.getInteger(index);
+        return value == null ? 0 : value;
+    }
+
+    public Short getShort(int index) {
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Short) {
+            return (Short) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).shortValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? Short.parseShort(str) : null;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to short");
+        }
+    }
+
+    public short getShortValue(int index) {
+        Short value = this.getShort(index);
+        return value == null ? 0 : value;
     }
 
     public Boolean getBoolean(int index) {
-        Object value = getOrNull(index);
-        return value == null ? null : JsonUtil.toJavaObject(value, Boolean.class);
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Boolean) {
+            return (Boolean) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).intValue() == 1;
+        } else if (!(value instanceof String)) {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to boolean");
+        } else {
+            String str = (String) value;
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? "true".equalsIgnoreCase(str) || "1".equals(str) : null;
+        }
+    }
+
+    public boolean getBooleanValue(int index) {
+        Boolean value = this.getBoolean(index);
+        return value != null && value;
+    }
+
+    public BigDecimal getBigDecimal(int index) {
+        Object value = this.get(index);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Number) {
+            if (value instanceof BigDecimal) {
+                return (BigDecimal) value;
+            } else if (value instanceof BigInteger) {
+                return new BigDecimal((BigInteger) value);
+            } else if (value instanceof Float) {
+                float floatValue = (Float) value;
+                return new BigDecimal(floatValue);
+            } else if (value instanceof Double) {
+                double doubleValue = (Double) value;
+                return new BigDecimal(doubleValue);
+            } else {
+                long longValue = ((Number) value).longValue();
+                return BigDecimal.valueOf(longValue);
+            }
+        } else if (value instanceof String) {
+            return new BigDecimal(((String) value).trim());
+        } else if (value instanceof Boolean) {
+            return (Boolean) value ? BigDecimal.ONE : BigDecimal.ZERO;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to BigDecimal");
+        }
     }
 
     public <T> T getObject(int index, Class<T> clazz) {
--- schemax-code/src/main/java/org/wsitm/schemax/utils/json/JSONObject.java
+++ schemax-code/src/main/java/org/wsitm/schemax/utils/json/JSONObject.java
@@ -3,9 +3,11 @@ package org.wsitm.schemax.utils.json;
 import com.fasterxml.jackson.core.type.TypeReference;
 import org.wsitm.schemax.utils.JsonUtil;
 
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.math.BigDecimal;
+import java.math.BigInteger;
+import java.text.SimpleDateFormat;
+import java.time.temporal.TemporalAccessor;
+import java.util.*;
 
 public class JSONObject extends LinkedHashMap<String, Object> {
     public JSONObject() {
@@ -40,22 +42,200 @@ public class JSONObject extends LinkedHashMap<String, Object> {
 
     public String getString(String key) {
         Object value = get(key);
-        return value == null ? null : String.valueOf(value);
+        if (value == null) {
+            return null;
+        } else if (value instanceof String) {
+            return (String) value;
+        } else if (value instanceof Date) {
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+            return sdf.format((Date) value);
+        } else {
+            return !(value instanceof Boolean)
+                    && !(value instanceof Character)
+                    && !(value instanceof Number)
+                    && !(value instanceof UUID)
+                    && !(value instanceof Enum)
+                    && !(value instanceof TemporalAccessor) ? JsonUtil.toJSONString(value) : value.toString();
+        }
     }
 
-    public Integer getInteger(String key) {
+    public Double getDouble(String key) {
         Object value = get(key);
-        return value == null ? null : JsonUtil.toJavaObject(value, Integer.class);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Double) {
+            return (Double) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).doubleValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? Double.parseDouble(str) : null;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to double");
+        }
+    }
+
+    public double getDoubleValue(String key) {
+        Double value = this.getDouble(key);
+        return value == null ? (double) 0.0F : value;
+    }
+
+    public Float getFloat(String key) {
+        Object value = super.get(key);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Float) {
+            return (Float) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).floatValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? Float.parseFloat(str) : null;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to float");
+        }
+    }
+
+    public float getFloatValue(String key) {
+        Float value = this.getFloat(key);
+        return value == null ? 0.0F : value;
     }
 
     public Long getLong(String key) {
-        Object value = get(key);
-        return value == null ? null : JsonUtil.toJavaObject(value, Long.class);
+        Object value = super.get(key);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Long) {
+            return (Long) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).longValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            if (!str.isEmpty() && !"null".equalsIgnoreCase(str)) {
+                return str.indexOf(46) != -1 ? (long) Double.parseDouble(str) : Long.parseLong(str);
+            } else {
+                return null;
+            }
+        } else if (value instanceof Boolean) {
+            return (Boolean) value ? 1L : 0L;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to Long");
+        }
+    }
+
+    public long getLongValue(String key) {
+        return this.getLongValue(key, 0L);
+    }
+
+    public long getLongValue(String key, long defaultValue) {
+        Long value = getLong(key);
+        return value == null ? defaultValue : value;
+    }
+
+    public Integer getInteger(String key) {
+        Object value = super.get(key);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Integer) {
+            return (Integer) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).intValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            if (!str.isEmpty() && !"null".equalsIgnoreCase(str)) {
+                return str.indexOf(46) != -1 ? (int) Double.parseDouble(str) : Integer.parseInt(str);
+            } else {
+                return null;
+            }
+        } else if (value instanceof Boolean) {
+            return (Boolean) value ? 1 : 0;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to Integer");
+        }
+    }
+
+    public int getIntValue(String key) {
+        return this.getIntValue(key, 0);
+    }
+
+    public int getIntValue(String key, int defaultValue) {
+        Integer value = getInteger(key);
+        return value == null ? defaultValue : value;
+    }
+
+    public Short getShort(String key) {
+        Object value = super.get(key);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Short) {
+            return (Short) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).shortValue();
+        } else if (value instanceof String) {
+            String str = ((String) value).trim();
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? Short.parseShort(str) : null;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to short");
+        }
+    }
+
+    public short getShortValue(String key) {
+        Short value = this.getShort(key);
+        return value == null ? 0 : value;
     }
 
     public Boolean getBoolean(String key) {
-        Object value = get(key);
-        return value == null ? null : JsonUtil.toJavaObject(value, Boolean.class);
+        Object value = super.get(key);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Boolean) {
+            return (Boolean) value;
+        } else if (value instanceof Number) {
+            return ((Number) value).intValue() == 1;
+        } else if (!(value instanceof String)) {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to boolean");
+        } else {
+            String str = (String) value;
+            return !str.isEmpty() && !"null".equalsIgnoreCase(str) ? "true".equalsIgnoreCase(str) || "1".equals(str) : null;
+        }
+    }
+
+    public boolean getBooleanValue(String key) {
+        Boolean value = this.getBoolean(key);
+        return value != null && value;
+    }
+
+    public boolean getBooleanValue(String key, boolean defaultValue) {
+        Boolean value = this.getBoolean(key);
+        return value == null ? defaultValue : value;
+    }
+
+    public BigDecimal getBigDecimal(String key) {
+        Object value = super.get(key);
+        if (value == null) {
+            return null;
+        } else if (value instanceof Number) {
+            if (value instanceof BigDecimal) {
+                return (BigDecimal) value;
+            } else if (value instanceof BigInteger) {
+                return new BigDecimal((BigInteger) value);
+            } else if (value instanceof Float) {
+                float floatValue = (Float) value;
+                return new BigDecimal(floatValue);
+            } else if (value instanceof Double) {
+                double doubleValue = (Double) value;
+                return new BigDecimal(doubleValue);
+            } else {
+                long longValue = ((Number) value).longValue();
+                return BigDecimal.valueOf(longValue);
+            }
+        } else if (value instanceof String) {
+            return new BigDecimal(((String) value).trim());
+        } else if (value instanceof Boolean) {
+            return (Boolean) value ? BigDecimal.ONE : BigDecimal.ZERO;
+        } else {
+            throw new JSONException("Can not cast '" + value.getClass() + "' to BigDecimal");
+        }
     }
 
     public <T> T getObject(String key, Class<T> clazz) {
@@ -70,7 +250,7 @@ public class JSONObject extends LinkedHashMap<String, Object> {
 
     @Override
     public JSONObject clone() {
-        return JsonUtil.parseObject(this.toJSONString());
+        return new JSONObject(this);
     }
 
     public Object to() {
